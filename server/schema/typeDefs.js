2ddadb172279bbc872a7af4d6ec4a9969c6e32c2
danielnels <danieln@newground.net.au>
2022-05-17 20:32:57 +0800

    Updated Models

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -28,8 +28,7 @@ const typeDefs = gql`
         user(userId: ID!): User
     }
     type Mutation {
-        getS3Url(isLoggedIn: Boolean!): String!
-        getS3UrlAuthenticated(isLoggedIn: Boolean!): String!
+       
         addUser(username: String!, email: String!, password: String!): Auth
         login(email: String!, password: String!): Auth
 
