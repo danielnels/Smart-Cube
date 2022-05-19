83e971770efe587f2b09a23838f9715c771c0cc8
danielnels <danieln@newground.net.au>
2022-05-19 18:52:01 +0800

    updated connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,15 +1,13 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
 
-//get mongoose connection to mongodb locally or via provided URI
+module.exports = mongoose.connection;
+const mongoose = require('mongoose');
+
 mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost/" + process.env.DB_NAME,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        // useCreateIndex: true,
-        // useFindAndModify: false,
-    }
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/react-router-activity',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
 );
 
 module.exports = mongoose.connection;
