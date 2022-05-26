68ede74b0e6952c1f494c06a43fab5ad82512fee
danielnels <danieln@newground.net.au>
2022-05-26 15:02:16 +0800

    Updated Data Bsse

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 //get mongoose connection to mongodb locally or via provided URI
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/" + smartcube_db,
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/smartcube_db",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
